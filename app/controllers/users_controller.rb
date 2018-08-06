4c0c4b58393e6484e65af8b339bb981b657cb65f
allisonkadel <owner@allisonkadel.com>
2018-08-06 12:48:11 -0700

    build users controller new action and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = user.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path
+        else
+            render :new
+        end
+    end
+
 end
