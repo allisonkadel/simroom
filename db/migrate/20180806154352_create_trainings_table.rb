e63c7e420e3f5f869679fc36dd4436dcbab4965f
allisonkadel <owner@allisonkadel.com>
2018-08-06 08:44:56 -0700

    create trainings migration table

---
+++ db/migrate/20180806154352_create_trainings_table.rb
@@ -0,0 +1,12 @@
+class CreateTrainingsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :trainings do |t|
+      t.string :name
+      t.text :description
+      t.date :date
+      t.string :simroom
+      t.integer :user_id
+      t.integer :equipment_id
+    end
+  end
+end
