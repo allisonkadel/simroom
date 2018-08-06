82506c772e37dd63455e330b960d5a7433046f1f
allisonkadel <owner@allisonkadel.com>
2018-08-06 14:12:49 -0700

    build new and create actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(:email => params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to new_session_path
+        end
+    end
+
 end
