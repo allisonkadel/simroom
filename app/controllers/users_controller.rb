4922d06b2f1ae7761e7a683aaae7938adbfa85f8
allisonkadel <owner@allisonkadel.com>
2018-08-06 13:31:54 -0700

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = user.new(user_params)
+        @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to user_path
+            redirect_to user_path(@user)
         else
             render :new
         end
