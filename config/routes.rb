6bd6eb8853ae58b1c425ad4ba0bdd5ca76886522
allisonkadel <owner@allisonkadel.com>
2018-08-06 14:11:55 -0700

    add sessions resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :equipment
   resources :trainings
   resources :users
+  resources :sessions
 
   root 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
