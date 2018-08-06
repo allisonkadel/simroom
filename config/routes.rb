0e1140b9e64da2634b0330764037c36605bab48e
allisonkadel <owner@allisonkadel.com>
2018-08-06 13:26:35 -0700

    add root resource, static#home, and add login link to root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :equipment
   resources :trainings
   resources :users
+
+  root 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
