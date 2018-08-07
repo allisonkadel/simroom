2638a45cf8f2db449f21f36203804ecedb194862
allisonkadel <owner@allisonkadel.com>
2018-08-07 12:14:19 -0700

    build current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
         !!current_user
     end
 
+    private
 
+        def current_user
+            @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
 
 end
