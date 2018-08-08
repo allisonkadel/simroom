c803e61050abfcdf74a12dbd4514320ffcf3d63e
allisonkadel <owner@allisonkadel.com>
2018-08-08 13:18:36 -0700

    add auth route for sessions controller create action to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resources :sessions
 
   root 'static#home'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
