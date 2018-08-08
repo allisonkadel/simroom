946b656a7526450391e7ad6fd9ecac57a75ce9dc
allisonkadel <owner@allisonkadel.com>
2018-08-08 10:08:30 -0700

    fix typo

--- app/models/training.rb
+++ app/models/training.rb
@@ -3,7 +3,7 @@ class Training < ApplicationRecord
     belongs_to :equipment
 
     validates :name, :presence => true
-    valiates :name, uniqueness => true
+    validates :name, :uniqueness => true
     validates :description, :presence => true
     validates :date, :presence => true
     validates :simroom, :presence => true
