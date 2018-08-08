da67d76129941657ea0bdcacb49d54a633e71689
allisonkadel <owner@allisonkadel.com>
2018-08-08 09:53:21 -0700

    add validations

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,10 @@
 class Training < ApplicationRecord
     belongs_to :user
     belongs_to :equipment
+
+    validates :name, :presence => true
+    valiates :name, uniqueness => true
+    validates :description, :presence => true
+    validates :date, :presence => true
+    validates :simroom, :presence => true
 end
