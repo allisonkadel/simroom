105271b681e8cb75a59601a04678858750c3a111
allisonkadel <owner@allisonkadel.com>
2018-08-08 05:20:36 -0700

    build trainings controller create action

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -8,4 +8,13 @@ class TrainingsController < ApplicationController
         @training = Training.new
     end
 
+    def create
+        @training = Training.new(training_params)
+        if @training.save
+            redirect_to training_path(@training)
+        else
+            render :new
+        end
+    end
+
 end
