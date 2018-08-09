bafa047078835a3d6b729ed92d3fecd878ea5983
allisonkadel <owner@allisonkadel.com>
2018-08-09 10:59:53 -0700

    build user class method find_or_create_by_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, :uniqueness => true
+
+    def self.find_or_create_by_omniauth(auth_hash)
+        self.where(:email => auth_hash["info"]["nickname"]).first_or_create do |user|
+            user.password = SecureRandom.hex
+        end
+    end
 end
