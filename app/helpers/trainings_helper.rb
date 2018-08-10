81b67cfae772fe6f5d8df84ae570426fbead62a7
allisonkadel <owner@allisonkadel.com>
2018-08-10 09:12:26 -0700

    build admin_edit_button and admin_cancel_button helpers for training show page

--- app/helpers/trainings_helper.rb
+++ app/helpers/trainings_helper.rb
@@ -1,2 +1,13 @@
 module TrainingsHelper
+
+    def admin_edit_button
+        button_to "Edit Training", edit_training_path, 
+        method: :get if current_user.id == @training.user_id
+    end
+
+    def admin_cancel_button
+        button_to "Cancel Training", {action: "destroy", id: @training.id},
+        method: :delete if current_user.id == @training.user_id
+    end
+
 end
